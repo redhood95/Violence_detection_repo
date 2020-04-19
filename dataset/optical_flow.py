fd3b3403470af7cdf4ef137d9c02a66f8dc013ae
yash upadhyay <yashup1997@gmail.com>
2020-04-20 01:05:26 +0530

    flow calculation

--- dataset/optical_flow.py
+++ dataset/optical_flow.py
@@ -6,11 +6,35 @@ import cv2
 
 Hockey_path  = "..\data\HockeyFights\extracted_frames"
 
-H_yes_path = Hockey_path +"\yes"
-H_no_path = Hockey_path +"\ono"
+H_list  = os.listdir(Hockey_path)
+for category in H_list:
+    frame_folders = os.listdir(os.path.join(Hockey_path,category))
+    for seq in frame_folders:
+        frame_path = os.path.join(os.path.join(Hockey_path,category),seq)
+        frames = os.listdir(frame_path)
+        for frame_num in range(1,len(frames)-1):
+            #load frame 1#
+            frame1 = cv2.imread(frame_path+"\\"+str(frame_num)+".png")
+            frame2 = cv2.imread(frame_path+"\\"+str(frame_num+1)+".png")
+            hsv = np.zeros_like(frame1)
+            hsv[...,1] = 255
+            flow = cv.calcOpticalFlowFarneback(frame1,frame2, None, 0.5, 3, 15, 3, 5, 1.2, 0)
 
 
 # for violent flows # 
-Vf_path  = "..\data\\violent_flows\extracted_frames"
-vf_yes_path = Vf_path +"\yes"
-vf_no_path = Vf_path +"\ono"
+Vf_path  = "..\data\\violentflows\extracted_frames"
+vf_list  = os.listdir(Vf_path)
+for category in vf_list:
+    frame_folders = os.listdir(os.path.join(Vf_path,category))
+    for seq in frame_folders:
+        frame_path = os.path.join(os.path.join(Vf_path,category),seq)
+        frames = os.listdir(frame_path)
+        for frame_num in range(1,len(frames)-1):
+            #load frame 1#
+            frame1 = cv2.imread(frame_path+"\\"+str(frame_num)+".png")
+            frame2 = cv2.imread(frame_path+"\\"+str(frame_num+1)+".png")
+            hsv = np.zeros_like(frame1)
+            hsv[...,1] = 255
+            flow = cv.calcOpticalFlowFarneback(frame1,frame2, None, 0.5, 3, 15, 3, 5, 1.2, 0)
+
+
