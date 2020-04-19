58b0850e2eb71b574c7ca5297c2ec84136af9e5f
yash upadhyay <yashup1997@gmail.com>
2020-04-19 23:29:11 +0530

    frame extraction for violent flows dataset

--- dataset/frame_extract_vf.py
+++ dataset/frame_extract_vf.py
@@ -1,3 +1,18 @@
 import os 
 import cv2
 
+path  = "..\data\violentflows\movies"
+
+
+os.mkdir("..\data\violentflows\extracted_frames")
+os.mkdir("..\data\violentflows\extracted_frames\yes")
+os.mkdir("..\data\violentflows\extracted_frames\ono")
+
+for i in range(0,5):
+    no_path = os.path.join(os.path.join(path,str(i)),"NonViolence")
+    yes_path = os.path.join(os.path.join(path,str(i)),"Violence")
+
+    yes =  os.listdir(yes_path)
+    no  =  os.listdir(no_path)
+
+
