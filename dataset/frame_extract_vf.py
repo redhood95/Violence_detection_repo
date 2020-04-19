f9b307de35fab6d5187aa844515ce277aecc1ba2
yash upadhyay <yashup1997@gmail.com>
2020-04-19 23:39:54 +0530

    frame extraction for positives

--- dataset/frame_extract_vf.py
+++ dataset/frame_extract_vf.py
@@ -9,10 +9,42 @@ os.mkdir("..\data\violentflows\extracted_frames\yes")
 os.mkdir("..\data\violentflows\extracted_frames\ono")
 
 for i in range(0,5):
+    print("==================================" + str(i)+"==================================")
     no_path = os.path.join(os.path.join(path,str(i)),"NonViolence")
     yes_path = os.path.join(os.path.join(path,str(i)),"Violence")
 
     yes =  os.listdir(yes_path)
     no  =  os.listdir(no_path)
+
+    for k in range(0,len(yes)):
+        print("yes===================" +str(k) )
+        folder_path = os.path.join("..\data\violentflows\extracted_frames\yes",str(i)+"__"+str(k))
+        os.mkdir(folder_path)
+        vid_path  = os.path.join(yes_path,yes[k])
+        cap = cv2.VideoCapture(vid_path)
+        count = 0
+        while(cap.isOpened()):
+            ret, frame = cap.read()
+            
+            if ret == True:
+                count = count + 1
+                image_path = os.path.join(folder_path,str(count))
+                cv2.imwrite(image_path+".png",frame)
+                if cv2.waitKey(1) & 0xFF == ord('q'):
+                    break
+            else:
+                break
+        print("number of frames extracted :"+str(count))
+        cap.release()
+
+
+
+
+    for k in range(0,len(no)):
+        print("no===================" +str(k) )
+
+
+
+    
 
 
