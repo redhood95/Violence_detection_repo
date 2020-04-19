3b8c166d0cdd34a91f761e22e707a17b5c1433c9
yash upadhyay <yashup1997@gmail.com>
2020-04-20 00:30:09 +0530

    paths

--- dataset/optical_flow.py
+++ dataset/optical_flow.py
@@ -1,2 +1,16 @@
 import os 
 import cv2
+
+
+# for hockey fights # 
+
+Hockey_path  = "..\data\HockeyFights\extracted_frames"
+
+H_yes_path = Hockey_path +"\yes"
+H_no_path = Hockey_path +"\ono"
+
+
+# for violent flows # 
+Vf_path  = "..\data\\violent_flows\extracted_frames"
+vf_yes_path = Vf_path +"\yes"
+vf_no_path = Vf_path +"\ono"
