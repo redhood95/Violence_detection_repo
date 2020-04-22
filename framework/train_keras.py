3a75a61e9328836517eebab6d3435bfee05f3558
yash upadhyay <yashup1997@gmail.com>
2020-04-22 19:48:57 +0530

    saving / loading model

--- framework/train_keras.py
+++ framework/train_keras.py
@@ -5,7 +5,9 @@ import os.path
 warnings.filterwarnings("ignore")
 sys.path.insert(1, 'D:\\res2020\Computer_Vision\\violence_detect\\root')
 from keras.callbacks import TensorBoard, ModelCheckpoint, EarlyStopping, CSVLogger
-from models.keras_models import ResearchModels
+from models.keras_models import Models
+from keras.models import model_from_json
+
 class Train_using_keras:
     def train(self,X,Y,X_eval,Y_eval,seq_length, model, saved_model=None,
           class_limit=None, image_shape=None,
@@ -34,11 +36,11 @@ class Train_using_keras:
         steps_per_epoch = 64
 
         # Get the model.
-        rm = ResearchModels(2, model, seq_length, saved_model)
+        m = Models(2, model, seq_length, saved_model)
 
         # Fit!
 
-        rm.model.fit(
+        m.model.fit(
             X,
             Y,
             batch_size=batch_size,  
@@ -46,6 +48,22 @@ class Train_using_keras:
             verbose=1,
             callbacks=[tb, early_stopper, csv_logger],
             epochs=nb_epoch)
+
+        model_json = m.model.to_json()
+        with open("saved_models/lrcn_10_v1.json", "w") as json_file:
+            json_file.write(model_json)
+        # serialize weights to HDF5
+        model.save_weights("saved_models/lrcn_10_v1.h5")
+        print("Saved model to disk")
+
+        # load json and create model
+        json_file = open('saved_models/lrcn_10_v1.json', 'r')
+        loaded_model_json = json_file.read()
+        json_file.close()
+        loaded_model = model_from_json(loaded_model_json)
+        # load weights into new model
+        loaded_model.load_weights("saved_models/lrcn_10_v1.h5")
+        print("Loaded model from disk")
 
 
 
