c2baeab58970ae1a713806f377fbae5d6b30b5bf
yash upadhyay <yashup1997@gmail.com>
2020-04-28 01:44:30 +0530

    all  frames in single list

--- dataset/violent_flows.py
+++ dataset/violent_flows.py
@@ -8,7 +8,8 @@ class Violent_flows:
     def load_data(self,path):
         v_list  = os.listdir(path)
 
-        All_vids = []
+        y_vid = []
+        n_vid = []
         #loading original sequence
         for category in v_list:
             print(category)
@@ -18,41 +19,34 @@ class Violent_flows:
                 frame_path = os.path.join(os.path.join(path,category),seq)
                 frames = os.listdir(frame_path)
                 num_frames_present = len(frames)
-                vid = []
                 for frame_num in range(1,len(frames)-1):
                     #load frame 1#
                     frame1 = cv2.imread(frame_path+"\\"+str(frame_num)+".png")
                     frame1 = cv2.resize(frame1,(50,100))
-                    vid.append(frame1)
-                empty = np.zeros_like(frame1)
-                for i in range(num_frames_present,50):
-                    vid.append(empty)
+                    if category == v_list[0]:
+                        y_vid.append(frame1)
+                    elif category == v_list[1]:
+                        n_vid.append(frame1)
 
-                All_vids.append(vid)
-
+        y_vid = np.array(y_vid)
+        n_vid = np.array(n_vid)
+        print(len(y_vid))
+        print(len(n_vid))
+        
         print('original sequence loaded')
 
-        All_vids = np.array(All_vids)
-        anno = []
-
-        for i in range(0,len(All_vids)):
-            if i < (len(All_vids)/2):
-                anno.append(0)
-            else :
-                anno.append(1)
-
-        anno = np.array(anno)
+   
 
 
-        train_x , x , train_y , y = train_test_split(All_vids , anno , 
-                                            test_size = 0.2 ,
-                                            random_state = 324)
+        # train_x , x , train_y , y = train_test_split(All_vids , anno , 
+        #                                     test_size = 0.2 ,
+        #                                     random_state = 324)
 
-        eval_x , test_x , eval_y , test_y = train_test_split(x , y , 
-                                                            test_size = 0.5 , 
-                                                            random_state = 324)
+        # eval_x , test_x , eval_y , test_y = train_test_split(x , y , 
+        #                                                     test_size = 0.5 , 
+        #                                                     random_state = 324)
 
-        return train_x,train_y,eval_x  , eval_y, test_x , test_y
+        # return train_x,train_y,eval_x  , eval_y, test_x , test_y
 
 
 vf = Violent_flows()
