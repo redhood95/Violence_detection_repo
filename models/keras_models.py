8fc54fd9e094098f34c17fbf51231b562177cf79
yash upadhyay <yashup1997@gmail.com>
2020-04-28 00:43:32 +0530

    3D convolutional network

--- models/keras_models.py
+++ models/keras_models.py
@@ -7,10 +7,9 @@ from keras.layers.convolutional import (Conv2D, MaxPooling3D, Conv3D,MaxPooling2
 from keras.layers import BatchNormalization, Activation
 from collections import deque
 from keras import regularizers
-
 import sys
 
-class ResearchModels():
+class Models():
     def __init__(self, nb_classes, model, seq_length,
                  saved_model=None, features_length=1024):
 
@@ -28,15 +27,15 @@ class ResearchModels():
         if self.saved_model is not None:
             print("Loading model %s" % self.saved_model)
             self.model = load_model(self.saved_model)
-        elif model == 'lstm':
-            print("Loading LSTM model.")
-            self.input_shape = (seq_length, features_length)
-            self.model = self.lstm()
         elif model == 'lrcn':
             print("Loading CNN-LSTM model.")
             self.input_shape = (seq_length, 100, 50, 3)
             self.model = self.lrcn()
 
+        elif model == 'c3d':
+            print("loading conv 3d model")
+            self.input_shape = (seq_length, 100, 50, 3)
+            self.model = self.C3D()
         # Now compile the network.
         optimizer = Adam(lr=1e-5, decay=1e-6)
         self.model.compile(loss='categorical_crossentropy', optimizer=optimizer,
@@ -44,30 +43,12 @@ class ResearchModels():
 
         print(self.model.summary())
 
-    def lstm(self):
-        """Build a simple LSTM network. We pass the extracted features from
-        our CNN to this model predomenently."""
-        # Model.
-        model = Sequential()
-        model.add(LSTM(2048, return_sequences=False,
-                       input_shape=self.input_shape,
-                       dropout=0.5))
-        model.add(Dense(512, activation='relu'))
-        model.add(Dropout(0.5))
-        model.add(Dense(self.nb_classes, activation='softmax'))
 
-        return model
 
     def lrcn(self):
 
         def add_default_block(model, kernel_filters, init, reg_lambda):
 
-            # conv
-            # model.add(TimeDistributed(Conv2D(kernel_filters, (3, 3), padding='same',
-            #                                  kernel_initializer=init, kernel_regularizer=regularizers.l2(l=reg_lambda))))
-            # model.add(TimeDistributed(BatchNormalization()))
-            # model.add(TimeDistributed(Activation('relu')))
-            # conv
             model.add(TimeDistributed(Conv2D(kernel_filters, (3, 3), padding='same',
                                              kernel_initializer=init, kernel_regularizer=regularizers.l2(l=reg_lambda))))
             model.add(TimeDistributed(BatchNormalization()))
@@ -88,16 +69,9 @@ class ResearchModels():
                                   input_shape=self.input_shape))
         model.add(TimeDistributed(BatchNormalization()))
         model.add(TimeDistributed(Activation('relu')))
-        # model.add(TimeDistributed(Conv2D(32, (3,3), kernel_initializer=initialiser, kernel_regularizer=regularizers.l2(l=reg_lambda))))
-        # model.add(TimeDistributed(BatchNormalization()))
-        # model.add(TimeDistributed(Activation('relu')))
-        # model.add(TimeDistributed(MaxPooling2D((2, 2), strides=(2, 2))))
 
-        # 2nd-5th (default) blocks
+
         model = add_default_block(model, 64,  init=initialiser, reg_lambda=reg_lambda)
-        # model = add_default_block(model, 128, init=initialiser, reg_lambda=reg_lambda)
-        # model = add_default_block(model, 256, init=initialiser, reg_lambda=reg_lambda)
-        # model = add_default_block(model, 512, init=initialiser, reg_lambda=reg_lambda)
 
         # LSTM output head
         model.add(TimeDistributed(Flatten()))
@@ -105,6 +79,32 @@ class ResearchModels():
         model.add(Dense(self.nb_classes, activation='softmax'))
 
         return model
+
+    def C3D(self):
+
+        # Model.
+        model = Sequential()
+        model.add(Conv3D(
+            32, (3,3,3), activation='relu', input_shape=self.input_shape
+        ))
+        model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
+        model.add(Conv3D(64, (3,3,3), activation='relu'))
+        model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
+        model.add(Dropout(0.7))
+        model.add(Conv3D(128, (3,3,3), activation='relu'))
+        model.add(Conv3D(128, (3,3,3), activation='relu'))
+        model.add(MaxPooling3D(pool_size=(1, 2, 2), strides=(1, 2, 2)))
+        model.add(Dropout(0.7))
+        model.add(Conv3D(256, (1,1,1), activation='relu'))
+        model.add(Conv3D(256, (1,1,1), activation='relu'))
+
+        model.add(Flatten())
+        # model.add(Dense(128))
+        model.add(Dropout(0.7))
+        model.add(Dense(self.nb_classes, activation='softmax'))
+
+        return model
+
 
 
 
